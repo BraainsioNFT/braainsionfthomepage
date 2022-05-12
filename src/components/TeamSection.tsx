48b8d2db5697b7a21b7954a383e128657b678a3a
Adrastopoulos <gabriel.hall@cox.net>
2022-05-11 21:31:43 -0700

    fix paths

--- src/components/TeamSection.tsx
+++ src/components/TeamSection.tsx
@@ -9,20 +9,20 @@ export const TeamSection: FC = () => {
       <div className="mt-10 max-w-[1000px] grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
         <TeamCard
           name="Qing Agar"
-          img="/qingagar.png"
+          img="qingagar.png"
           role="Lead"
           description="Loves crypto and genuine NFTs. Hates rugpulls with a passion."
         />
-        <TeamCard name="Phantasma" img="/phantasma.png" role="Game Dev" />
-        <TeamCard name="Cleito.art" img="/cleito.png" role="Artist" />
+        <TeamCard name="Phantasma" img="phantasma.png" role="Game Dev" />
+        <TeamCard name="Cleito.art" img="cleito.png" role="Artist" />
         <TeamCard
           name="Adrastopoulous"
-          img="/adrastopoulous.png"
+          img="adrastopoulous.png"
           role="Web Dev"
           description="Avid cryptologist and web developer."
         />
-        <TeamCard name="m0dE" img="/m0dE.png" role="Advisor" />
-        <TeamCard name="AetherWindZ" img="/aetherwindz.png" role="Advisor" />
+        <TeamCard name="m0dE" img="m0dE.png" role="Advisor" />
+        <TeamCard name="AetherWindZ" img="aetherwindz.png" role="Advisor" />
       </div>
     </section>
   );
