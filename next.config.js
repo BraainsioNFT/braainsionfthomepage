850068fdeac079ef5a10f4a675df39687178616a
QiNG-agar <axbigot@gmail.com>
2022-05-11 19:10:05 -0700

    feat: gh pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig = {
   reactStrictMode: true,
+  assetPrefix: isProd ? '/braainsionfthomepage/' : '',
 }
 
 module.exports = nextConfig
