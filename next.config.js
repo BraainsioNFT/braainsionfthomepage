75818151e597a8023f2c5af61a79624ba19f09a6
QiNG-agar <axbigot@gmail.com>
2022-05-17 21:46:09 -0700

    fix: assetPrefix is invalid for about.braains.io

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const isProd = process.env.NODE_ENV === "production";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: isProd ? "/braainsionfthomepage" : "",
+  assetPrefix: isProd ? "" : "",
 };
 
 module.exports = nextConfig;
