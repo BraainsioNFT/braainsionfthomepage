5c08bfcbc4a2ab68f0ac738b95b3a10a9ae5eb9b
QiNG-agar <QiNG-agar@users.noreply.github.com>
2022-05-18 05:18:04 +0000

    deploy: ed3861e2e521724296db144f98615117c49f61ea

--- _next/static/PJqsLr57LMEKmIEiiSVf_/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/545f34e4-dbc17c10cfc09d79.js","static/chunks/1bfc9850-d22aa0cc8629a883.js","static/chunks/pages/index-4f6f207b4a54041e.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
