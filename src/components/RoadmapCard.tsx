75131f073c65c0d05053a5f16bec3a0a769ad896
QiNG-agar <axbigot@gmail.com>
2022-05-22 22:14:11 -0700

    test: merge

--- src/components/RoadmapCard.tsx
+++ src/components/RoadmapCard.tsx
@@ -9,14 +9,14 @@ const RoadmapCard = (props: RoadmapCardProps) => {
 			className={`${props.isDisplayed? "w-[768px] h-[560px]" : "w-96"} flex flex-col items-center p-3 font-acme relative`}
 			data-aos={props.isDisplayed? "" : "zoom-in-left"}
 		>
-			<div className={`w-[90%] ${props.isDisplayed? "h-full" : "h-64"} relative transition overflow-hidden shadow-drop bg-emerald-300 rounded-md ${props.isHovered && !props.isDisplayed? "blur" : ""}`}>
+			<div className={`w-[90%] ${props.isDisplayed? "h-full" : "h-64"} relative transition overflow-hidden shadow-drop bg-green-700 bg-opacity-75 rounded-md ${props.isHovered && !props.isDisplayed? "blur" : ""}`}>
 				<button onClick={() => {
 					props.setDisp(0);
 
 					// aos not updating. brute fix
 					window.scrollBy({ left: 0, top: 1});
 					window.scrollBy({ left: 0, top: -1});
-				}} className={`${props.isDisplayed? "absolute top-5 right-5 rounded-full border-2 border-green-900 text-green-900 hover:bg-green-900 hover:text-white h-10 aspect-square" : "hidden"}`}>
+				}} className={`${props.isDisplayed? "absolute top-5 right-5 rounded-full border-2 border-stone-900 text-stone-900 hover:bg-stone-900 hover:text-white h-10 aspect-square" : "hidden"}`}>
 					X
 				</button>
 				<h1 className={`h-16 text-4xl ${props.isDisplayed? "py-5" : "py-2"}`}>{props.title ?? "Stage I: Outbreak"}</h1>
@@ -25,7 +25,7 @@ const RoadmapCard = (props: RoadmapCardProps) => {
 						<img src={props.icon ?? "/0.png"} className="absolute left-0 right-0 top-0 bottom-0 m-auto"></img>
 					</div>
 					<div className={props.isDisplayed? "w-full relative px-2" : "w-1/2 relative px-2"}>
-						<p className={`absolute top-0 bottom-0 my-auto h-fit ${props.isDisplayed? "px-20" : ""}`}>
+						<p className={`absolute top-0 bottom-0 my-auto h-fit ${props.isDisplayed? "px-20" : "px-5"}`}>
 							{
 								props.isDisplayed?
 								props.dispDesc ?? '- item 1\n- item 2'
@@ -36,7 +36,7 @@ const RoadmapCard = (props: RoadmapCardProps) => {
 				</div>
 			</div>
 			<div className={!props.isDisplayed && props.isHovered? "left-0 right-0 bottom-0 top-0 m-auto h-64 absolute flex items-center justify-center" : "hidden"}>
-				<button className="rounded-lg shadow-md border-green-900 text-green-900 border-2 text-3xl hover:bg-green-900 hover:text-white transition p-2"
+				<button className="rounded-lg shadow-md border-stone-900 text-stone-900 border-2 text-3xl hover:bg-stone-900 hover:text-white transition p-2"
 				onClick={() => {
 					props.setDisp(props.id)
 				}} >
--- src/components/RoadmapSection.tsx
+++ src/components/RoadmapSection.tsx
@@ -15,20 +15,17 @@ export const RoadmapSection: FC = () => {
     return "";
   };
 
-  const checklistify = (items: { content: string; isDone?: boolean }[]) => {
-    const list: JSX.Element[] = [];
+  const Checklistify = (items: { content: string, isDone?: boolean }[]) => {
+    const list: JSX.Element[] = []
+
+    const [expanded, setExpanded] = useState(-1);
 
     for (const item of items) {
       list.push(
-        <div
-          className="flex w-fit relative py-2 items-center"
-          key={items.indexOf(item)}
-        >
-          <img
-            src={item.isDone ? "checked.png" : "unchecked.png"}
-            className="h-5 aspect-square"
-          ></img>
-          <p className="pl-5">{item.content}</p>
+        <div className="flex w-fit relative py-2 items-center text-left" key={items.indexOf(item)}>
+          <img src={item.isDone? "checked.png" : "unchecked.png"} className="h-5 aspect-square"></img>
+          <p className="pl-5 hover:text-blue-500 hover:cursor-pointer">{item.content}</p>
+          <p className="text-sm"></p>
         </div>
       );
     }
@@ -44,105 +41,27 @@ export const RoadmapSection: FC = () => {
     >
       <SectionTitle title="ROADMAP" />
       <div className="w-full flex justify-center items-center">
-        <div
-          className={`mt-10 ${
-            disp === 0 ? "grid grid-cols-1 lg:grid-cols-2" : "h-[560px]"
-          }`}
-        >
-          <div
-            className={hideConditionally(1)}
-            onMouseOver={() => {
-              setHover(1);
-            }}
-            onMouseLeave={() => {
-              setHover(0);
-            }}
-          >
-            <RoadmapCard
-              dispDesc={checklistify([
-                {
-                  content: "Launch Site, Roadmap, and Demo Game",
-                  isDone: true,
-                },
-                { content: "Build up network of VIPs", isDone: false },
-                {
-                  content: "Start WhiteList programs and giveaways",
-                  isDone: false,
-                },
-                {
-                  content:
-                    "Partner with other NFT projects (WhiteList convergence)",
-                  isDone: false,
-                },
-                { content: "Secondary marketplace approval", isDone: false },
-                { content: "VIP mint at 1.00 SOL", isDone: false },
-                {
-                  content:
-                    "WhiteList mint a day or two before public mint at 1.25 SOL",
-                  isDone: false,
-                },
-                { content: "Public mint at 1.50 SOL" },
-              ])}
-              id={1}
-              setDisp={setDisp}
-              isHovered={hover === 1}
-              isDisplayed={disp === 1}
-              title="Stage I: Outbreak"
-              desc="The apocalypse begins with the launch of 5,000 unique survivor BraainsioNFTs."
-            />
+        <div className={`mt-10 ${disp === 0? "grid grid-cols-1 lg:grid-cols-2" : "h-[560px]"}`}>
+          <div className={hideConditionally(1)} onMouseOver={() => {
+            setHover(1);
+          }} onMouseLeave={() => {
+            setHover(0);
+          }} >
+            <RoadmapCard icon={'/outbreakicon.png'} dispDesc={Checklistify([{content: "Launch Site, Roadmap, and Demo Game", isDone: true}, {content: "Build up network of VIPs", isDone: false}, {content: "Start WhiteList programs and giveaways", isDone: false}, {content: "Partner with other NFT projects (WhiteList convergence)", isDone: false}, {content: "Secondary marketplace approval", isDone: false}, {content: "VIP mint at 1.00 SOL", isDone: false}, {content: "WhiteList mint a day or two before public mint at 1.25 SOL", isDone: false}, {content: "Public mint at 1.50 SOL"}])} id={1} setDisp={setDisp} isHovered={hover === 1} isDisplayed={disp === 1} title="Stage I: Outbreak" desc="The apocalypse begins with the launch of 5,000 unique survivor BraainsioNFTs." />
           </div>
-          <div
-            className={hideConditionally(2)}
-            onMouseOver={() => {
-              setHover(2);
-            }}
-            onMouseLeave={() => {
-              setHover(0);
-            }}
-          >
-            <RoadmapCard
-              dispDesc={checklistify([
-                {
-                  content: "NFT ingame skins integration (connect wallet)",
-                  isDone: false,
-                },
-                {
-                  content: "Trails and animations implementation",
-                  isDone: false,
-                },
-              ])}
-              id={2}
-              setDisp={setDisp}
-              isHovered={hover === 2}
-              isDisplayed={disp == 2}
-              title="Stage II: Survival"
-              desc="Survivors face the zombie horde ingame as the NFT is integrated in several ways."
-            />
+          <div className={hideConditionally(2)} onMouseOver={() => {
+            setHover(2);
+          }} onMouseLeave={() => {
+            setHover(0);
+          }} >
+            <RoadmapCard icon={"/survivalicon.png"} dispDesc={Checklistify([{content: "NFT ingame skins integration (connect wallet)", isDone: false}, {content: "Trails and animations implementation", isDone: false}])} id={2} setDisp={setDisp} isHovered={hover === 2} isDisplayed={disp == 2} title="Stage II: Survival" desc="Survivors face the zombie horde ingame as the NFT is integrated in several ways." />
           </div>
-          <div
-            className={hideConditionally(3)}
-            onMouseOver={() => {
-              setHover(3);
-            }}
-            onMouseLeave={() => {
-              setHover(0);
-            }}
-          >
-            <RoadmapCard
-              dispDesc={checklistify([
-                {
-                  content:
-                    "Subsequent Zombie and Alpha Zombie collections minted and integrated under the same model as survivors",
-                  isDone: false,
-                },
-              ])}
-              id={3}
-              setDisp={setDisp}
-              isHovered={hover === 3}
-              isDisplayed={disp == 3}
-              title="Stage III: Mutation"
-              desc="A new threat arises to the survivors with the release of 5,000 unique zombie BraainsioNFTs."
-            />
+          <div className={hideConditionally(3)} onMouseOver={() => {
+            setHover(3);
+          }} onMouseLeave={() => {
+            setHover(0);
+          }} >
+            <RoadmapCard icon="/mutationicon.png" dispDesc={Checklistify([{content: "Subsequent Zombie and Alpha Zombie collections minted and integrated under the same model as survivors", isDone: false}])} id={3} setDisp={setDisp} isHovered={hover === 3} isDisplayed={disp == 3} title="Stage III: Mutation" desc="A new threat arises to the survivors with the release of 5,000 unique zombie BraainsioNFTs." />
           </div>
         </div>
       </div>
