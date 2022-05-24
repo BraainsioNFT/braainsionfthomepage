beed4c8a683423e1aee2038865a4052c95a3378d
QiNG-agar <axbigot@gmail.com>
2022-05-24 14:08:03 -0700

    feat: solidified FAQ questions and answers

--- src/components/FAQSection.tsx
+++ src/components/FAQSection.tsx
@@ -5,35 +5,27 @@ import { SectionTitle } from "./SectionTitle";
 
 const FAQs: Array<{ question: string; answer: string }> = [
   {
-    question: "Why add an NFT to braains?",
+    question: "Why create a NFT for Braains?",
     answer:
-      "NFT communities are really strong and filled with dedicated members because these members have money invested in the product. We are adding NFTs to braains to build a community of supporters just as strong as our current playerbase (i.e. expanding while maintaining quality).",
+      "NFT communities are really strong and filled with dedicated members who believe in projects enough to have money invested in it. We are adding NFTs to braains to build a community of supporters just as strong as our current playerbase. We are growing our community without sacrificing its quality.",
   },
   {
     question: "What is the current status of the NFT?",
     answer:
-      "We're working full time on NFT. There's a lot of moving parts and we're still early and figuring things out. The NFTs are currently planned to start being sold in July/August (2-3 months). So yeah im not kidding when I say we're early.. Which makes everyone here a OG in the BraainsNFT timeline 🎉",
-  },
-  {
-    question:
-      "What is the plan is the NFT blockchain fails? Will braains development continue?",
-    answer:
-      "It's unlikely blockchain will fail (there might be dips and crashes but overall it's not going everywhere). But if it did fail, braains would still continue, yes. We've been developing braains for 6 years without blockchain already,",
-  },
-  {
-    question: "So these NFT skins will be bought with crypto?",
-    answer: `Yes NFT skins will be bought with crypto. Think of it rather than "NFT skin" but instead a NFT that is a skin ingame (since there will be more than just getting a skin to buying braainsNFT). Existing skins will remain in old braains version of game while nft version of braains will be created alongside (the two will be very different so it's not like 2 clones).`,
+      "We are working full time on NFT. There's a lot of moving parts and we're still early and figuring things out. The NFTs are currently planned to start being sold in July/August (2-3 months). So we're not kidding when we say you're early.. Which makes everyone reading this a OG in the BraainsNFT timeline 🎉",
   },
   {
     question: "What will the price be?",
-    answer: "The price will be about 1-1.5 SOL depending on market conditions.",
+    answer: "The exact price will be determined based on market conditions as we approach mint day. For us, it's more important to have many holders than it is to make more money as dedicated holders are infinitely more valuable than a couble extra funding when it comes to growth.",
   },
   {
     question: "Can I earn crypto?",
     answer:
-      "Yes, this is play2earn which is coming in moddio in the coming months. You will earn Modd Tokens, which will be worth real money. Play 2 earn, create 2 earn: you make money making games based on number of players and you make money playing games through creators' incentives (i.e. a creator will give X tokens to top players of the week in order to attract new players).",
+      "Yes, a play-to-earn model is coming soon to Modd.io, Braains.io's parent site and partner. Through this system, players will be able to receive crypto from completion of ingame challenges, events, and highscores. This system will be quite flexible and allow us to take the input of the community as to how you would like our tokens to be earned.",
   },
-  { question: "When can I start earning?", answer: "July/August." },
+  { question: "When is the mint?", answer: "July/August 2022" },
+  { question: "When can I Play-to-Earn?", answer: "Anticipated early 2023" },
+  { question: "Where can I contact the team if I still have questions?", answer: "You can contact us on discord at discord.gg/braainsio or contact the project lead directly at QiNG-agar#0540"}
 ];
 
 export const FAQSection: FC = () => {
