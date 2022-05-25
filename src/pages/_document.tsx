854d2a672f61743ce20c2e5dbbc5b65bf661374e
Adrastopoulos <gabriel.hall@cox.net>
2022-05-25 16:13:43 -0700

    metadata

---
+++ src/pages/_document.tsx
@@ -0,0 +1,26 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <meta property="og:title" content="Braains.io NFT" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://about.braains.io" />
+        <meta
+          property="og:image"
+          content="https://about.braains.io/nftgif.gif"
+        />
+        <meta
+          property="og:description"
+          content="The official NFT for the hit zombie survival browser game"
+        />
+        <meta name="theme-color" content="#32e0a3" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
